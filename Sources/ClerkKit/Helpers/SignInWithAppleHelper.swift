97bd15dc83c548f035a93bf90b42a49685ab0aa0
William Klein <bill@onesmallsky.com>
2026-04-21 08:31:50 -0600

    Fixed issue with iOS Sign in with Apple presentation anchor

--- Sources/ClerkKit/Helpers/SignInWithAppleHelper.swift
+++ Sources/ClerkKit/Helpers/SignInWithAppleHelper.swift
@@ -104,12 +104,19 @@ extension SignInWithAppleHelper: ASAuthorizationControllerPresentationContextPro
   @MainActor
   func presentationAnchor(for _: ASAuthorizationController) -> ASPresentationAnchor {
     #if os(iOS)
-    UIApplication.shared.windows.first(where: { $0.isKeyWindow }) ?? ASPresentationAnchor()
+    let scene = UIApplication.shared.connectedScenes
+      .compactMap { $0 as? UIWindowScene }
+      .first(where: { $0.activationState == .foregroundActive })
+      ?? UIApplication.shared.connectedScenes
+        .compactMap { $0 as? UIWindowScene }
+        .first
+    return scene?.keyWindow ?? scene?.windows.first ?? ASPresentationAnchor()
     #else
     ASPresentationAnchor()
     #endif
   }
 }
+
 
 extension ASAuthorizationAppleIDCredential {
   var tokenString: String {
